43350e95372d95de88727e71cfb21ef11d75620d
odilbek29 <kamoliddinpardaev272@gmail.com>
2025-02-18 19:09:22 +0500

    Add files via upload

---
+++ odilbe.js
@@ -0,0 +1,19 @@
+let ism=prompt("ismingizni kiriting");
+let Fam=prompt("Familyangizni kiriting");
+let yosh=prompt("yoshingizni kiriting");
+let yashash=prompt("Tural joyingizni kiriting");
+let ish=prompt("Qayerda ishlaysiz ");
+let bola=prompt("Uylangan misiz yoki turmishga chiqan misaiz kiriting");
+let mashina=prompt("mashinayiz bormi nomi qanaqa kiriting");
+alert("Malumot uchun raxmat!!");
+
+
+
+
+console.log("Sizning ismingiz: "+ ism);
+console.log("Sizning familaygiz: "+ Fam);
+console.log("Sizning yoshingiz: "+ yosh);
+console.log("Sizning turar joyingiz: "+ yashash);
+console.log("Sizning ishngiz: "+ ish);
+console.log("Sizning uylangan misiz yoki turmishga chqan: "+ bola);
+console.log("Sizning mashinayiz bormi nomi qanaqa: "+ mashina);
